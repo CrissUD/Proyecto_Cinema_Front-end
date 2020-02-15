8d8082c75e6fff5094aab998d228b4c0e3f7e826
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-02-15 18:27:23 -0500

    se remueve video de fondo que no se estaba utilizando

--- src/components/cartelera/Cartelera.js
+++ src/components/cartelera/Cartelera.js
@@ -3,14 +3,25 @@ import PeliculaCartelera from './PeliculaCartelera'
 
 export default class Cartelera extends Component {
 
+    state = {
+        list_movies: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+    }
     mostrarPeliculas = () =>{
-        
+        return(
+            <div className="cartel_movies">
+                {this.state.list_movies.map(movies => (
+                    <PeliculaCartelera
+                        key={movies}
+                    /> 
+                ))}
+            </div>
+        )   
     }
 
     render() {
         return (
             <div className="container">
-                <PeliculaCartelera/>
+                {this.mostrarPeliculas()}
             </div>
         )
     }
